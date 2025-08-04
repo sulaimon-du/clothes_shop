5d7136ab6f2d512fba3de4d52b0e6219f2985f94
Sulaimon <sulaimon.gh@gmail.com>
2025-08-04 17:43:25 +0500

    made views/ urls/ admin

--- mmain/models.py
+++ mmain/models.py
@@ -49,7 +49,8 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
-    class ProductImage(models.Model):
-        product = models.ForeignKey('Product', on_delete=models.CASCADE, 
-                                    related_name='images')
-        image = models.ImageField(upload_to='product/extra/')
+
+class ProductImage(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, 
+                                related_name='images')
+    image = models.ImageField(upload_to='product/extra/')
